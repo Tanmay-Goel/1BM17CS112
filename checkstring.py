318814e5325aa23012846bd92a73035cfe22f984
Tanmay-Goel <53892924+Tanmay-Goel@users.noreply.github.com>
2019-10-10 12:26:10 +0530

    Add files via upload

---
+++ checkstring.py
@@ -0,0 +1,37 @@
+class checkstr:
+    def __init__(self):
+        pass
+
+    def check(self, a):
+        stack=[]
+        top=-1
+        l=len(a)
+        sb=['(', '{', '[']
+        eb=[')', '}', ']']
+        for i in a:
+            if i in eb and top==-1:
+                return False
+            stack.append(i)
+            top+=1;
+            if i in eb:
+                b=eb.index(i)
+                if sb[b]== stack[top-1]:
+                    del(stack[top])
+                    del(stack[top-1])
+                    top-=2
+                else:
+                    return False
+                
+        return True       
+                
+
+a=input("Enter string pattern: ")
+obj=checkstr()
+b=obj.check(a)
+if(b==True):
+    print("\nValid")
+else:
+    print("\nInvalid")
+            
+
+
